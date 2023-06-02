a06076b2a05f377a4c3c362f1b234d9fc812bb0e
BAXT27 <127516114+BAXT27@users.noreply.github.com>
2023-06-02 09:06:36 -0400

    Consumo de API Pública_CDI0101
    Bastian Mayorga y Renato Leon

---
+++ Evaluacion_N°2_CCNA_DEVNET.py
@@ -0,0 +1,29 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?" 
+key = "86921dJhej1HQPIu4oZ53yedL86RpAcI"
+
+while True:
+    orig = input("Ciudad de origen: ")
+    if orig == "quit" or orig == "q":
+        break
+    dest = input("Ciudad de destino: ")
+    if dest == "quit" or dest == "q":
+        break
+    url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+    print("URL: " + (url))
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+        print("=============================================")
+        print("Directions from " + (orig) + " to " + (dest))
+        print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+        print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+        print("=============================================")
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+           print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+        print("=============================================\n")
+
+
